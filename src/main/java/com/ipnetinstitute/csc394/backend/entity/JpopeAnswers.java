08566aa703e46401c5a2134d221331887e4bed4b
wisdomtohe <elmanciowisdom@gmail.com>
2020-09-03 20:29:59 +0000

    Version for JPOPE

---
+++ src/main/java/com/ipnetinstitute/csc394/backend/entity/JpopeAnswers.java
@@ -0,0 +1,24 @@
+package com.ipnetinstitute.csc394.backend.entity;
+
+public class JpopeAnswers extends BaseEntity{
+    private String courseSurvey;
+    private String mayaki;
+    private String ahoeGnoe;
+    private String piigo;
+    private String doctorHelp;
+    private String detectionIntrusion;
+    private String ciscoIse;
+    private String toiP;
+    private String siem;
+    private String nessus;
+    private int cS;
+    private int mk;
+    private int aG;
+    private int pg;
+    private int dH;
+    private int dI;
+    private int cI;
+    private int tiP;
+    private int sm;
+    private int ns;
+}
